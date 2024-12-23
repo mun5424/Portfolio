5a3d4d9d67f618a676ff60c9753f6da6e3a592a8
mun5424 <mun5424@gmail.com>
2024-12-23 00:18:04 -0800

    add font, typing animation

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,19 +1,27 @@
-import { Component } from "react";
 import './../style/About.css'
 import portrait from '../assets/portrait.jpg'
 
-export default class About extends Component {
-    render() {
-    return <section id="about">
-    <div className="about-container">
+const About = () => {
+    return <div className="about-container">
       <div className="about-details">
         <div className="about-title"> 
           <h1 className="title main-title">
             Hi,<br/> I am Charlie Oh
           </h1>
+          <div className="typewriter">
           <h3 className="title sub-title">
-          currently focused on building software to solve real-world problems.
+            Software Engineer and Full-Stack Developer 
           </h3>
+          </div>
+          <div className="about-paragraph">
+          <p>
+            I am an avid programmer, collaborator and listener. I like building software to solve problems - I consider software as an art in that anyone can exercise many ways to solve one problem.
+            <br />
+            I have experience with various object-oriented programming languages. I've worked with backend technologies such as Java, Python, and C#, and frontend technologies such as Angular, Node, and React.
+            <br />
+            On my free time, I like playing video games, scoping out new places for delicious food, and practice piano.
+          </p>
+          </div>
         </div>
         <div className="about-me-buttons">
           <button type="button" className="button">More About Me</button>
@@ -25,7 +33,7 @@ export default class About extends Component {
           <img src={portrait} />
         </div>
       </div>
-    </div>
-  </section>
-    }
+  </div>
 }
+
+export default About;
