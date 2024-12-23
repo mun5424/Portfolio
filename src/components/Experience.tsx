048929fecda5235af8538158666b6d50c3d751cc
mun5424 <mun5424@gmail.com>
2024-12-22 20:06:57 -0800

    style up experience page and add rest of exp

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,6 +1,10 @@
 import { Component } from "react";
 import ExperienceCard from "./ExperienceCard";
 import '../style/Experience.css'
+import transceptalogo from '../assets/transcepta.jpg'
+import amazonlogo from '../assets/amazon.jpg'
+import cofebelogo from '../assets/cofebe.png'
+import bristolfarmslogo from '../assets/bristolfarms.png'
 
 const Experience = () => {
     return <div className="experience-container">
@@ -11,7 +15,32 @@ const Experience = () => {
             techStack={["C#", "Azure DevOps", "ASP.NET", "SQL Server", "Visual Studio"]}
             startDate="Jan 2024"
             endDate="Nov 2024" 
-            />
+            logo={transceptalogo}
+        />
+        <ExperienceCard
+            jobName="Software Development Engineer"
+            companyName="Amazon"
+            techStack={["Java", "Python", "React", "GraphQL", "DynamoDB", "AWS"]}
+            startDate="Sept 2021"
+            endDate="May 2023" 
+            logo={amazonlogo}
+        />
+        <ExperienceCard
+            jobName="Software Engineer"
+            companyName="COFEBE"
+            techStack={["Java", "Python", "C#", "AngularJS", "Typescript", "SQL Server", "Visual Studio"]}
+            startDate="Aug 2018"
+            endDate="Mar 2021" 
+            logo={cofebelogo}
+        />
+        <ExperienceCard
+            jobName="Programmer Analyst"
+            companyName="Bristl Farms"
+            techStack={["SQL Server", "Visual Basic .NET"]}
+            startDate="Jan 2015"
+            endDate="Aug 2017" 
+            logo={bristolfarmslogo}
+        />
     </div>
 
 }
--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import classNames from 'classnames';
 import '../style/Experience.css'
 
 interface ExperienceCardProps {
@@ -7,21 +7,53 @@ interface ExperienceCardProps {
     techStack: string[];
     startDate: string;
     endDate: string;
+    logo: string;
   }
+
+const colorMap = new Map<string, string>([
+    ["C#", "csharpblue"],  
+    ["SQL Server", "sqlserver"],  
+    ["Visual Studio", "visualstudio"],  
+    ["Azure DevOps", "azuredevops"],  
+    ["ASP.NET", "aspnet"],  
+    ["Java", "java"],  
+    ["Python", "python"],  
+    ["React", "react"],  
+    ["GraphQL", "graphql"],  
+    ["DynamoDB", "dynamodb"],  
+    ["AWS", "aws"],  
+    ["Visual Basic .NET", "vbnet"],  
+    ["Typescript", "typescript"],  
+    ["AngularJS", "angularjs"],  
+])
 
 function getTechStacks( techStack: string[])  {
     if (techStack.length > 0) {      
         return techStack.map((item, index) => (
-            <span key={index}>{item} </span>
+            <span className={`${colorMap.get(item)} skill-tag`} key={index}>{item} </span>
         ));
     }
 }
 
-function ExperienceCard({ jobName, companyName, techStack, startDate, endDate }: ExperienceCardProps) {
+
+function ExperienceCard({ jobName, companyName, techStack, startDate, endDate, logo }: ExperienceCardProps) {
     return <div className="experience-card border-gradient border-gradient-purple" > 
-        <h3>{jobName}</h3>
-        <h4>{companyName}</h4>
+        <div className="experience-title">
+            <div className="experience-image">
+                <img className="company-logo" src={logo} />
+            </div>
+            <div className="experience-job-name">
+                <h3>{jobName}</h3>
+                <h4>{companyName}</h4>
+            </div>
+            <div className="start-end-dates">
+                <p>{startDate} ~ {endDate}</p>
+            </div>
+        </div>
+        <div className="skill-tags">
         {getTechStacks(techStack)}
+        </div>
+        
     </div>
 }
 
