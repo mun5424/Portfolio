86334dbe9bfa23c7df7b6671060cee19cb53a2ea
Charlie Oh <mun5424@gmail.com>
2024-12-23 20:20:49 -0800

    adding mobile responsive wip

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -16,9 +16,9 @@ const About = () => {
           <div className="about-paragraph">
           <p>
             I am an avid programmer, collaborator and listener. I like building software to solve problems - I consider software as an art in that anyone can exercise many ways to solve one problem.
-            <br />
+            <br /><br />
             I have experience with various object-oriented programming languages. I've worked with backend technologies such as Java, Python, and C#, and frontend technologies such as Angular, Node, and React.
-            <br />
+            <br /><br />
             On my free time, I like playing video games, scoping out new places for delicious food, and practice piano.
           </p>
           </div>
